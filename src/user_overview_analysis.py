8e6472f0309cbffa7ee616de00cf590956e9fcca
Hunegn <demissiehunegna@gmail.com>
2024-12-20 14:17:05 +0300

    Top 10 handsets

--- src/user_overview_analysis.py
+++ src/user_overview_analysis.py
@@ -14,9 +14,17 @@ def explore_data(data):
     
     print("\nDescriptive Statistics:")
     print(data.describe())
+def handsets(data):
+    top_handsets = data['Handset Type'].value_counts().head(10)
+    print("\nTop 10 Handsets:")
+    print(top_handsets)
 
+
+  
 if __name__ == "__main__":
     file_path = '../data/Copy of Week2_challenge_data_source(CSV).csv'
     data = load_data(file_path)
     explore_data(data)
+    handsets(data)
+    
 
